e4d52a9aa0f6c5dbceda6a5afcbc21b44bf40c88
ciiska5 <zayacvpole34@gmail.com>
2023-06-18 14:14:03 +0300

    style: убрать лишние импорты

--- src/main/java/ru/practicum/shareit/item/dao/InMemoryItemDaoImpl.java
+++ src/main/java/ru/practicum/shareit/item/dao/InMemoryItemDaoImpl.java
@@ -4,8 +4,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import ru.practicum.shareit.exception.ItemNotFoundException;
-import ru.practicum.shareit.item.dto.ItemDto;
-import ru.practicum.shareit.item.mapper.ItemMapper;
 import ru.practicum.shareit.item.model.Item;
 
 import java.util.*;
