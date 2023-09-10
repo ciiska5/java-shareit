41a94423dfbcf7a417081df10b745c292d09b5cc
ciiska5 <zayacvpole34@gmail.com>
2023-09-10 07:33:47 +0300

    style: убрать неиспользуемые импорты

--- gateway/src/main/java/ru/practicum/shareit/item/comment/dto/CommentRequestDto.java
+++ gateway/src/main/java/ru/practicum/shareit/item/comment/dto/CommentRequestDto.java
@@ -5,7 +5,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
-import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
--- gateway/src/main/java/ru/practicum/shareit/request/dto/ItemRequestRequestDto.java
+++ gateway/src/main/java/ru/practicum/shareit/request/dto/ItemRequestRequestDto.java
@@ -3,11 +3,8 @@ package ru.practicum.shareit.request.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import ru.practicum.shareit.item.dto.ItemRequestDto;
 
 import javax.validation.constraints.NotBlank;
-import java.time.LocalDateTime;
-import java.util.List;
 
 /**
  * Шаблон объекта ItemRequest для пользователя
